7f3cae38c275d7bc0fdb4030677c0665381580da
Moisés Alejandro Navarro Presas <moisalejandro@gmail.com>
2020-02-14 16:34:59 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -29,7 +29,7 @@
     'author': "Humanytek",
     'website': "http://www.humanytek.com",
     'category': 'Ale',
-    'version': '1.9.1',
+    'version': '1.9.2',
     'depends': [
         'account',
         'sale',
